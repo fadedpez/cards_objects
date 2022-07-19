057095ea142574685a7ec63ea973119d9b254a52
frank <fvl7997@gmail.com>
2022-07-19 13:59:02 -0400

    Removed obsolete new deck function from card.go

--- entities/card.go
+++ entities/card.go
@@ -10,18 +10,3 @@ type Card struct {
 func (c *Card) String() string {
 	return fmt.Sprintf("%s of %s", c.Value, c.Suit)
 }
-
-func CreateDeck() []*Card {
-	suits := []string{"Hearts", "Spades", "Diamonds", "Clubs"}
-	values := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
-	deck := make([]*Card, 0)
-
-	for _, suit := range suits {
-		for _, value := range values {
-			deck = append(deck, &Card{Suit: suit, Value: value})
-		}
-	}
-
-	return deck
-
-}
